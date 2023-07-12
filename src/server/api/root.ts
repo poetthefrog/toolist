8a92415f53c1abe3b673d6191ff7634700dc5dd5
m1k3y <m1k3y@gHost.local>
2023-07-11 23:34:15 -0700

    start

---
+++ src/server/api/root.ts
@@ -0,0 +1,14 @@
+import { createTRPCRouter } from "~/server/api/trpc";
+import { linkRouter } from "./routers/link";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  link: linkRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
